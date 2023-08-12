9b8f4b3703c6e9b49f952d8b4413478bac32bb52
partha235 <parthaa235@gmail.com>
2023-08-12 21:51:59 +0530

    working

---
+++ dastr.r
@@ -0,0 +1,8 @@
+# vectors
+x <- c("hello", "hi", "how", "are", "you")
+# list
+d <- list("s", 56, "hello", c(5, 6, 3))
+# matrices
+q <- matrix(c(2, 6, 8, 4, "s", "e", "hello"), nrow = 3, ncol = 2, byrow = TRUE)
+# array
+w <- array(c(2, 6, 8, 4), dim = c())
---
+++ daty.r
@@ -0,0 +1,19 @@
+# integer
+y <- 5L
+print(class(y)) # class() to know the type.
+# numeric
+x <- 5
+w <- 5.2
+print(class(x))
+print(class(w))
+# complex
+d <- 5 + 2i
+print(class(d))
+# character
+r <- "hello"
+r2 <- "s"
+print(class(r))
+print(class(r2))
+# raw
+h <- charToRaw("hello")
+print(class(h))
