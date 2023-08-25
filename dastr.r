d7b1fb71a2df032dbe038387893bd6624b5ace8a
partha235 <parthaa235@gmail.com>
2023-08-25 20:02:43 +0530

    working

--- dastr.r
+++ dastr.r
@@ -1,8 +1,9 @@
 # vectors
-x <- c("hello", "hi", "how", "are", "you")
+x <- c("hello", "hi", "how", "are", "you", 3, 65)
 # list
 d <- list("s", 56, "hello", c(5, 6, 3))
 # matrices
-q <- matrix(c(2, 6, 8, 4, "s", "e", "hello"), nrow = 3, ncol = 2, byrow = TRUE)
+q <- matrix(c(2, 3, 4, "s", "e", "hello"), nrow = 3, ncol = 2, byrow = TRUE)
 # array
-w <- array(c(2, 6, 8, 4), dim = c())
+w <- array(c(2, 6, 8, 4), dim = c(1))
+print(w)
--- fucl.r
+++ fucl.r
@@ -1,4 +1,25 @@
-add_two.function <- function(x,y) {
-    cat("x+y= ",x+y,"\n")
+lift <- function(x) {
+    cat("your going to ", x, " floor\n")
 }
-add_two.function(5,6)
+# lift(6)
+pizza <- function(s_name, p_type)  {
+    cat("you have ordered", p_type, " pizza from", s_name, "\n")
+}
+# x <- readline("enter shop name\n")
+# y <- readline("enter pizza type\n")
+# pizza(s_name = x, p_type = y)
+
+
+hos_x <- function(p_name, p_diseases) {
+    if (p_diseases== "sugar") {
+        print("meeet doctor vasanthi")
+    } else if (p_diseases == "heart") {
+        print("meet doctor bala")
+    } else {
+        print("meet doctor hari")
+    }
+}
+
+f <- readline("enter patient name\n")
+g <- readline("enter diseases\n")
+hos_x(f, g)
